0c63524fe28dc948667bc893c15d5efafd01494f
LeeMinglu <402007353@qq.com>
2015-11-12 22:17:28 +0800

    项目初始化

---
+++ iOS_Lookback/iOS_Lookback/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_Lookback
+//
+//  Created by 李明禄 on 15/11/12.
+//  Copyright © 2015年 com.Socerer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
